a8d6026b43ff315c737854c4b93d259b7e648a38
Niels Witte <wrlxs@hotmail.com>
2021-12-14 11:58:22 +0100

    Day 14 part 2

--- 14/02.py
+++ 14/02.py
@@ -1,6 +1,6 @@
 import numpy as np
 from collections import Counter
-
+import tqdm
 
 with open('./14/data.txt', 'r') as f:
     data = [x.strip() for x in f.readlines()]
@@ -14,25 +14,55 @@ for l in data:
 
 print(template)
 
-print(len(reaction_map))
+groups = {}
+for x, y in zip(range(0, len(template) - 1), range(1, len(template))):
+    if template[x] + template[y] not in groups.keys():
+        groups[template[x] + template[y]] = 0
+    groups[template[x] + template[y]] += 1
 
-steps = 10
+print(groups)
+
+print(len(reaction_map))
+steps = 40
 for step in range(steps):
     reactions = []
-    for x, y in zip(range(0, len(template) - 1), range(1, len(template))):
-        # print(template[x] + template[y])
-        if template[x] + template[y] in reaction_map.keys():
-            reactions.append([template[x] + template[y], x])
-    adjustment = 0
-    for reactant, index in reactions:
-        template = template[:index + 1 + adjustment] + reaction_map[reactant] + template[index + 1 + adjustment:]
-        adjustment += 1
+    # Precalculate all of the reactions
+    for reactant, product in reaction_map.items():
+        if reactant in groups.keys():
+            # We need to add this later
+            reactions.append((reactant, product, groups[reactant]))
+
+    for reactant, product, amount in reactions:
+        groups[reactant] -= amount
+        # Create the child elements created by this reaction
+        a, b = reactant
+        a1 = a + product
+        b1 = product + b
+        if a1 not in groups.keys():
+            groups[a1] = 0
+        if b1 not in groups.keys():
+            groups[b1] = 0
+        groups[a1] += amount
+        groups[b1] += amount
 
 
-print(template)
-counts = dict(Counter(template))
+print(groups)
+
+counts = {}
+for k, v in groups.items():
+    a, b = k
+    if a not in counts.keys():
+        counts[a] = 0
+    if b not in counts.keys():
+        counts[b] = 0
+    counts[a] += v
+    counts[b] += v
+
+print(counts)
+
 counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
 
 print(counts)
 
 print(counts[0][1] - counts[-1][1])
+print(round((counts[0][1] - counts[-1][1]) / 2))
